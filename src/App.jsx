9a2c55b04b86a62e80fc4e1fbe03398507933ec6
vinixiii <vinissfigueiroa@gmail.com>
2023-07-25 14:40:08 -0300

    feat: create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import { Header } from './components/header';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>asdasd</main>
+      </div>
+    </>
+  );
 }
