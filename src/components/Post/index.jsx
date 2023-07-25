f03541b92d04854c00cd04d1d6c5a62143c8a67e
vinixiii <vinissfigueiroa@gmail.com>
2023-07-25 15:31:52 -0300

    feat: create post component

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,56 @@
+import styles from './styles.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/vinixiii.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Vinícius Figueiroa</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time
+          dateTime="2022-05-11 08:13:30"
+          title="11 de Maio de 2022 às 08:13h"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          <a href="#">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a> <a href="#">#nlw</a>{' '}
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
+}
